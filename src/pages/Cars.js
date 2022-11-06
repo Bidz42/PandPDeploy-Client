974a66a535fc603aa2baf84025c11e5209ffca72
Bidz42 <abid97r@gmail.com>
2022-11-06 12:30:55 +0000

    changes

--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -14,7 +14,7 @@ import {BsSquareFill} from 'react-icons/bs'
 
 
 const Cars = () => {
-  const server = "http://localhost:5005";
+  const server = "https://drab-red-woodpecker-hat.cyclic.app";
 
   const [cars, setCars] = useState([]);
 
